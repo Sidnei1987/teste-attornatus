433ba4f3d65add67c20c6fb224d9596e9460a122
Sidnei Santos Mendes <Sidnei1987@gmail.com>
2023-01-12 10:39:53 -0300

    teste backEnd

---
+++ src/main/java/com/TesteAttornatus/TesteAttornatus/TesteAttornatusApplication.java
@@ -0,0 +1,13 @@
+package com.TesteAttornatus.TesteAttornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAttornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAttornatusApplication.class, args);
+	}
+
+}
